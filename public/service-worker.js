e594da8fee4a7a6898a76382dc3294325e4e7d85
Alex McDonald <acm4219@gmail.com>
2020-11-13 23:28:01 -0600

    got the server changed due to conflicting service workers, trying to sort out the db part of it.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,14 @@
- const FILES_TO_CACHE = [
-     "/",
-     "/index.html",
-     "/styles.css",
-     "/index.js",
-     "/db.js",
-     "/manifest.webmanifest",
-     "/icons/icon-192x192.png",
-     "/icons/icon-512x512.png"
- ];
-
  const CACHE_NAME = "static-cache-v2";
  const DATA_CACHE_NAME = "data-cache-v1";
 
+ const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
+];
  self.addEventListener("install", function(evt) {
     evt.waitUntil(
         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
